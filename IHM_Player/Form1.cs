4eec09b56b118c9ec4fa478043c70afad30cd668
Hugo <hugo.ragiot@gmail.com>
2022-12-07 13:48:50 +0100

    display csv working

--- IHM_Player/Form1.cs
+++ IHM_Player/Form1.cs
@@ -15,39 +15,6 @@ namespace IHM_Player
 
         private void DisplayData_Click(object sender, EventArgs e)
         {
-
-            Data.Data.header = new List<string>()
-            {
-                "Id", "Name", "Last Name", "Paris"
-            };
-
-            Data.Data.csv = new List<List<string>>()
-            {
-                new List<string>() { "1", "John", "Doe", "Paris" },
-                new List<string>() { "2", "Jane", "Doe", "Paris" },
-                new List<string>() { "3", "John", "Doe", "Paris" },
-                new List<string>() { "4", "Jane", "Doe", "Paris" },
-                new List<string>() { "5", "John", "Doe", "Paris" },
-                new List<string>() { "6", "Jane", "Doe", "Paris" },
-                new List<string>() { "7", "John", "Doe", "Paris" },
-                new List<string>() { "8", "Jane", "Doe", "Paris" },
-                new List<string>() { "9", "John", "Doe", "Paris" },
-                new List<string>() { "10", "Jane", "Doe", "Paris" },
-                new List<string>() { "11", "John", "Doe", "Paris" },
-                new List<string>() { "12", "Jane", "Doe", "Paris" },
-                new List<string>() { "13", "John", "Doe", "Paris" },
-                new List<string>() { "14", "Jane", "Doe", "Paris" },
-                new List<string>() { "15", "John", "Doe", "Paris" },
-                new List<string>() { "16", "Jane", "Doe", "Paris" },
-                new List<string>() { "17", "John", "Doe", "Paris" },
-                new List<string>() { "18", "Jane", "Doe", "Paris" },
-                new List<string>() { "19", "John", "Doe", "Paris" },
-                new List<string>() { "20", "Jane", "Doe", "Paris" },
-                new List<string>() { "21", "John", "Doe", "Paris" },
-                new List<string>() { "22", "Jane", "Doe", "Paris" },
-                new List<string>() { "23", "John", "Doe", "Paris" },
-                new List<string>() { "24", "Jane", "Doe", "Paris" }
-            };
             
             Data.Data.header.ForEach(@v => dataGridView.Columns.Add(@v, @v));
             for (int i = 0; i < Data.Data.csv.Count; i++)
